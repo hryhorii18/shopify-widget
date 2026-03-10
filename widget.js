a5f3ad766002b746a76e76fa3c288014d9e13ba0
hryhorii18 <hryhorii.yalanskyi@gmail.com>
2026-03-10 15:02:17 +0200

    Add files via upload

---
+++ widget.js
@@ -0,0 +1,32 @@
+(async function(){
+
+    const productId = ShopifyAnalytics.meta.product.id;
+    const shop = Shopify.shop;
+
+    const res = await fetch(
+        "https://shop-app.kesug.com/api/purchases.php?product_id="+productId+"&shop="+shop
+    );
+
+    const data = await res.json();
+
+    let container = document.createElement("div");
+
+    container.style.marginTop="10px";
+
+    data.forEach(p=>{
+
+        let timeAgo = Math.floor((Date.now()-new Date(p.purchased_at))/60000);
+
+        let el=document.createElement("div");
+
+        el.innerHTML =
+            "✔ "+p.customer_name+
+            " purchased this "+timeAgo+" minutes ago";
+
+        container.appendChild(el);
+
+    });
+
+    document.querySelector('form[action="/cart/add"]').appendChild(container);
+
+})();
